e8885d5580cd8ab0e0a97121f7cbe35797fa8ddb
kkang <kkang104@daum.net>
2024-09-04 13:47:32 +0900

    test

---
+++ quiz-sol/2-1/지승현/main.js
@@ -0,0 +1,26 @@
+//다음의 함수는 1부터 10까지 숫자를 더하는 함수입니다.
+const sum = () => {
+    let b = 0;
+    for (i = 1; i <= 10; i++) {
+        b += i;
+        console.log("sum", b);
+    }
+}
+sum();
+
+
+//다음의 코드는 클래스를 생성하고 클래스의 함수를 호출하는 코드입니다.
+class User {
+    name = '홍길동';
+    sayHello = () => {
+        console.log(`hello, ${this.name}`);
+    }
+}
+new User().sayHello();
+
+//다음의 코드는 함수를 호출하여 상품을 추가하는 코드입니다.
+let productNumber = 0;
+const addProduct = () => {
+    console.log(`${productNumber} 상품을 추가했습니다.`);
+}
+addProduct();
---
+++ workspace/02-javascript-basic/02-1-getting start/test7-quiz/main.js
@@ -0,0 +1,27 @@
+//다음의 함수는 1부터 10까지 숫자를 더하는 함수입니다.
+const a=()=>{
+let b=0;
+for(i =1;i<=10;i++) {
+b+=i;
+console.log("sum",b);
+}
+};
+a();
+
+
+//다음의 코드는 클래스를 생성하고 클래스의 함수를 호출하는 코드입니다.
+class USER {
+NAME='홍길동'
+SAYHELLO=()=>{
+console.log(`hello, ${this.NAME}`)
+}
+}
+new USER().SAYHELLO()
+
+//다음의 코드는 함수를 호출하여 상품을 추가하는 코드입니다.
+let product_number = 0
+const add_product = () => {
+console.log(`${product_number} 상품을 추가했습니다.`)
+}
+add_product()
+
