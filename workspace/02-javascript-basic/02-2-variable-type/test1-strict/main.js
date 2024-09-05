0b9f7c0f034aa70c20511b185a52fd726b2d390b
kkang <kkang104@daum.net>
2024-09-05 09:31:26 +0900

    tesT

---
+++ workspace/02-javascript-basic/02-2-variable-type/test1-strict/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let data1 = 10
+data2 = 20
+
+console.log(data1, data2)
+
+function myFun() {
+  // "use strict";
+
+  let data3 = 10
+  data4 = 20
+  console.log(data3, data4)
+}
+myFun()
