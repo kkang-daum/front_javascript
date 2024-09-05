2a1403f027c9a969e460e764144bb385b54bdb91
kkang <kkang104@daum.net>
2024-09-05 11:04:54 +0900

    tesT

---
+++ quiz-sol/2-2-1/박소정/quiz/main.js
@@ -0,0 +1,9 @@
+const name = '박소정'
+var intro = "인터렉션 디밸로퍼를 꿈꾸는 박소정입니다."
+var wish = "성공적인 프로젝트를 수행하고 포트폴리오를 완성해 실력있는 개발자로 성장하고싶습니다."
+var message = "할 수 있다."
+
+document.write("이름: ", name, "<br>")
+document.write("자기소개: ", intro, "<br>")
+document.write("프론트엔드 스쿨 11기를 통해 이루고 싶은 점: ", wish, "<br>")
+document.write("동료들과 나에게 보내는 응원메세지: ", message, "<br>")
