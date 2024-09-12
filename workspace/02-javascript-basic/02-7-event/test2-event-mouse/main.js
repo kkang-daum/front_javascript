dfb22a2da085ca8084a8f49c441c75a850a184d1
kkang <kkang104@daum.net>
2024-09-12 11:49:15 +0900

    test

---
+++ workspace/02-javascript-basic/02-7-event/test2-event-mouse/main.js
@@ -0,0 +1,43 @@
+"use strict";
+
+window.addEventListener('load', () => {
+  //click, dblclick, down, up................
+  let button = document.getElementById('button')
+  button.addEventListener('click', () => {
+    console.log('mouse click')
+  })
+  button.addEventListener('dblclick', () => {
+    console.log('mouse double click')
+  })
+  button.addEventListener('mousedown', () => {
+    console.log('mouse down')
+  })
+  button.addEventListener('mouseup', () => {
+    console.log('mouse up')
+  })
+
+  //mouse move..............................
+  let area = document.getElementById('area')
+  let result = document.getElementById('result')
+
+  area.addEventListener('mousemove', (e) => {
+    result.innerHTML = `offset(${e.offsetX},${e.offsetY}, page(${e.pageX},${e.pageY}))`
+  })
+
+  //enter, over..................
+  let outer = document.getElementById('outer')
+  let inner = document.getElementById('inner')
+
+  outer.addEventListener('mouseenter', () => {
+    console.log('outer mouseenter')
+  })
+  inner.addEventListener('mouseenter', () => {
+    console.log('inner mouseenter')
+  })
+  outer.addEventListener('mouseover', () => {
+    console.log('outer mouseover')
+  })
+  inner.addEventListener('mouseover', () => {
+    console.log('inner mouseover')
+  })
+})
