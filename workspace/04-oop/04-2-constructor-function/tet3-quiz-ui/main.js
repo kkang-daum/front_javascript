d570b0d7e873c24df2e6b0bd00a138de9f8f5b20
kkang <kkang104@daum.net>
2024-09-25 10:50:10 +0900

    test

--- workspace/04-oop/04-2-constructor-function/tet3-quiz-ui/main.js
+++ workspace/04-oop/04-2-constructor-function/tet3-quiz-ui/main.js
@@ -50,9 +50,77 @@ let chatMainNode = document.getElementById('chat-main')
 
 //매개변수로 전달된 객체의 내용을 node 를 만들어서 출력시키는 역할.. 
 function printMessage(message){
+  //dropdown menu..........................
+  let menuImageNode = document.createElement('img')
+  menuImageNode.setAttribute('src', 'images/menu.jpg')
+  let menuButton = document.createElement('button')
+  menuButton.setAttribute('class', 'msg-info-menu dropbtn')
+  menuButton.appendChild(menuImageNode)
 
+  let link1 = document.createElement('a')
+  link1.setAttribute('href', '#')
+  //첫번째 - 이모지 추가 메시지 id, 두번째 - 추가해야 하는 이모지 id
+  link1.setAttribute('onclick', `emojiClick('${message.msgId}','thumbup')`)
+  let link1Text = document.createTextNode('좋아요')
+  link1.appendChild(link1Text)
+
+  let link2 = document.createElement('a')
+  link2.setAttribute('href', '#')
+  link2.setAttribute('onclick', `emojiClick('${message.msgId}','ok')`)
+  let link2Text = document.createTextNode('넵')
+  link2.appendChild(link2Text)
+
+  let links = document.createElement('div')
+  links.setAttribute('class', 'dropdown-content')
+  links.appendChild(link1)
+  links.appendChild(link2)
+
+  let dropdown = document.createElement('div')
+  dropdown.setAttribute('class', 'dropdown')
+  dropdown.appendChild(menuButton)
+  dropdown.appendChild(links)
+
+  //main data.....................
+  let name = document.createElement('div')
+  name.setAttribute('class', 'msg-info-name')
+  name.appendChild(document.createTextNode(message.member.nickname))
+
+  let date = document.createElement('div')
+  date.setAttribute('class', 'msg-info-time')
+  date.appendChild(document.createTextNode(message.date))
+
+  let msgInfo = document.createElement('div')
+  msgInfo.setAttribute('class', 'msg-info')
+  msgInfo.appendChild(name)
+  msgInfo.appendChild(date)
+  msgInfo.appendChild(dropdown)
+
+  //msg - text ...................
+  let msgText = document.createElement('div')
+  msgText.setAttribute('class', 'msg-text')
+  msgText.appendChild(document.createTextNode(message.msg))
+
+  let msgBubble = document.createElement('div')
+  msgBubble.setAttribute('class', 'msg-bubble')
+  msgBubble.appendChild(msgInfo)
+  msgBubble.appendChild(msgText)
+
+  //프사..............
+  let photoNode = document.createElement('img')
+  photoNode.setAttribute('src', message.member.photo)
+  photoNode.setAttribute('class', 'msg-img')
+
+  //화면에 메시지는 여러개 추가된다..
+  //각 div 태그를 id 로 식별해야 나중에 그 메시지에 이모지
+  //추가시 어느 div 에 출력해야 하는지 식별할 수 있다..
+  let mainNode = document.createElement('div')
+  mainNode.setAttribute('id', `msgId-${message.msgId}`)
+  mainNode.setAttribute('class', 'msg left-msg')
+  mainNode.appendChild(photoNode)
+  mainNode.appendChild(msgBubble)
+
+  chatMainNode.appendChild(mainNode)
 }
-
 function send(e){
   //유저 입력 데이터 추출.... 
   e.preventDefault()
@@ -66,7 +134,22 @@ function send(e){
     alert('아이디, 닉네임과 메시지를 입력해야 합니다.')
     return;
   }else {
-    
+    //유저 입력 데이터 화면에서 삭제..
+    idInputNode.value = ''
+    nicknameInputNode.value = ''
+    msgInputNode.value = ''
+
+    //메시지 입력 유저를 표현하는 객체 생성..
+    //프사는 유저 id 와 동일한 파일명.. 
+    let member = new Member(id, nickname, `images/${id}.jpg`)
+
+    //메시지 객체 생성..
+    let message = new Message(msg, member)
+
+    messages.push(message)
+
+    //메시지를 화면 출력.. 
+    printMessage(message)
   }
   //화면에 동적 노드 만들어서 출력..
 
